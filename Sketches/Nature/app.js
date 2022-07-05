b639e7c3a7bf483422089a4fa221f2034348cb8c
Molikkidd <mzkidd5@gmail.com>
2022-07-05 08:50:33 -0700

    add sun and grass

--- Sketches/Nature/app.js
+++ Sketches/Nature/app.js
@@ -15,7 +15,22 @@ function setup() {
 
   function draw() {
     background(ring.color());
-
+    // SKY
     fill(sky.color())
     rect(0,0,720,300)
+    // TREETOP
+    fill(treeTop.color())
+    triangle(400,400,100,400,250,250)
+    triangle(400,500,100,500,250,350)
+    triangle(400,550,100,550,250,450)
+    // TRUNK
+    fill(trunk.color())
+    rect(230,550,40,200)
+    // SUN
+    fill(sun.color())
+    ellipse(100,100,200,200)
+    fill(ring.color())
+    ellipse(100,100,140,140)
+    fill(colorPicker.color())
+    ellipse(100,100,80,80)
   }
