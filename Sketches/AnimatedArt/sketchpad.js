529896a1e1dde464db08a8f4dc022a9da78d9f1e
Molikkidd <mzkidd5@gmail.com>
2022-09-01 22:32:49 -0700

    sketchpad for initial drawing

---
+++ Sketches/AnimatedArt/sketchpad.js
@@ -0,0 +1,10 @@
+for (var x=100; x<=450; x +=160 ){
+      fill('#cb5382');
+      circle(171,167,20)
+      circle(155,172,20)
+      circle(145,162,20,)
+      circle(153,147,20)
+      circle(170,152,20)
+      fill('yellow')
+      circle(160,160,20)
+}
